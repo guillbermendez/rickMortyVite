2c82296c3db90b6b2490816ae513f5fba20b7fa5
Guillber Mendez <77417748+Guill-MH@users.noreply.github.com>
2022-08-03 17:54:56 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Guill-MH.github.io/rickMortyVite/",
+  base: "https://guillberdev.me/rickMortyVite/",
   plugins: [react()]
 })
